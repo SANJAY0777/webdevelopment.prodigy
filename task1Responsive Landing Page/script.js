3b99ebc26af858a78770bdd50c12eba4130b3109
SANJAY G <149760418+SANJAY0777@users.noreply.github.com>
2025-03-08 19:57:31 +0530

    Delete task1Responsive Landing Page/script.js

--- task1Responsive Landing Page/script.js
+++
@@ -1,9 +0,0 @@
-// Change Navbar Background on Scroll
-window.addEventListener("scroll", function() {
-    let navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
